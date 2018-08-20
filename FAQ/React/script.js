f5e2482d65b14bf513a4cbf76f44e064cf574fec
[Hamid] <heidarzadeh.hamidreza@gmail.com>
2018-08-20 16:05:10 -0400

    adding Form/Items Components

--- FAQ/React/script.js
+++ FAQ/React/script.js
@@ -8,9 +8,31 @@ var state = {
 
 
 //===========COMPONENTS===========
+var Form = function (props) {
+
+    var out =
+        ce('div', { className: props.cName });
+    return out;
+};
+
+var Items = function (props) {
+
+    var out =
+        ce('div', { className: props.cName });
+    return out;
+};
+
+var App = function (props) {
+    var out =
+        ce('div', { className: props.cName },
+            ce(Form, { cName: "form" }),
+            ce(Items, { cName: "items" })
+        );
+    return out;
+};
 
 //===== APP RENDERER =======
-var renderApp = function () { ReactDOM.render(ce('div', { className: "wrapper" }), document.getElementById('app')); };
+var renderApp = function () { ReactDOM.render(ce(App, { cName: "wrapper" }), document.getElementById('app')); };
 //Rendering App
 renderApp();
 
