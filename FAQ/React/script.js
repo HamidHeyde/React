3470ce6e05dafaff291081776355b388e5f9701b
[Hamid] <heidarzadeh.hamidreza@gmail.com>
2018-08-20 14:58:03 -0400

    Adding project files

---
+++ FAQ/React/script.js
@@ -0,0 +1,18 @@
+//===========FUNCTIONS============
+
+
+//===========GLOBALS============
+var ce = React.createElement;
+var state = {
+};
+
+
+//===========COMPONENTS===========
+
+//===== APP RENDERER =======
+var renderApp = function () { ReactDOM.render(ce('div', { className: "wrapper" }), document.getElementById('app')); };
+//Rendering App
+renderApp();
+
+
+
